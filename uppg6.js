3cb4dd468c983f24ff3df3476d25b53ef33fbda2
Jesper-beep <jesper.reijs.skola@gmail.com>
2025-05-09 20:25:05 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -5,6 +5,15 @@ function uppg6(){
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 
+    let namn = ["Jesper", "Göran", "Åke", "Berra"];
+
+    for (let i = 0; i < namn.lenght; i++)
+        {
+            console.log(namn[i]);
+
+        }
+
+
  
 }
 
