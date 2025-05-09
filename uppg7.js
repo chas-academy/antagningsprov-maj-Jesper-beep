b4b55e035286bfefad4ca46551ea55df30f11127
Jesper-beep <jesper.reijs.skola@gmail.com>
2025-05-09 20:37:00 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -5,6 +5,13 @@ function uppg7() {
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+   function addition(tal1, tal2)
+      {
+         return tal1 + tal2;
+      }
+
+   let resultat = addition(5,10);
+   console.log(resultat);
 
  
 }
